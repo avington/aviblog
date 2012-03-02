b44c4502d3abf5b1cd371249744c828c26b93168
Steven Moseley <avington12345@gmail.com>
2012-03-02 09:06:39 -0500

    #1: set up all the links on the header except for rss. will do that when feedburner is setup.

---
+++ AviBlog/AviBlog.Web/Controllers/ArchiveController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AviBlog.Web.Controllers
+{
+    public class ArchiveController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
