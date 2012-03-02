59889a54b7f37fe7176e198dfe47dab79b338372
Steven Moseley <avington12345@gmail.com>
2012-03-02 18:42:48 -0500

    #4: added archive post page

--- AviBlog/AviBlog.Web/Controllers/ArchiveController.cs
+++ AviBlog/AviBlog.Web/Controllers/ArchiveController.cs
@@ -1,12 +1,23 @@
 ﻿using System.Web.Mvc;
+using AviBlog.Core.Services;
+using AviBlog.Core.ViewModel;
 
 namespace AviBlog.Web.Controllers
 {
     public class ArchiveController : Controller
     {
+
+        private readonly IPostService _postService;
+
+        public ArchiveController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
         public ActionResult Index()
         {
-            throw new System.NotImplementedException();
+            PostListViewModel viewModel = _postService.GetAllPostForBlog();
+            return View(viewModel);
         }
     }
 }
