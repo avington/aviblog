101540778c48c99cc2f493d405717b2d8d4a1caf
Steven Moseley <avington12345@gmail.com>
2012-03-13 07:33:45 -0400

    nuget update

---
+++ AviBlog/AviBlog.Web/Content/assets/blog1/social_scripts.sql
@@ -0,0 +1,64 @@
+INSERT INTO [aviblog].[dbo].[Settings]
+           ([Key]
+           ,[Entry])
+     VALUES
+           ('TwitterTweetThisUrl'
+           ,'<a href="https://twitter.com/share" class="twitter-share-button" data-url="https://dev.twitter.com/pages/tweet_button" data-via="siravington" data-lang="en" data-count="vertical" data-related="siravington">Tweet</a>')
+GO
+
+INSERT INTO [aviblog].[dbo].[Settings]
+           ([Key]
+           ,[Entry])
+     VALUES
+           ('TwitterTweetthisScript'
+           ,'<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>')
+GO
+
+INSERT INTO [aviblog].[dbo].[Settings]
+           ([Key]
+           ,[Entry])
+     VALUES
+           ('FaceBookLikeScript'
+           ,'<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>')
+GO
+
+
+INSERT INTO [aviblog].[dbo].[Settings]
+           ([Key]
+           ,[Entry])
+     VALUES
+           ('FaceBookLikeUrl'
+           ,'<div class="fb-like" data-href="http://www.avingtonsolutions.com/Posts/Post/using-the-aspnet-mvc-jquery-template-to-repeat-a-h" data-send="false" data-layout="box_count" data-width="40" data-show-faces="true"></div>')
+GO
+
+INSERT INTO [aviblog].[dbo].[Settings]
+           ([Key]
+           ,[Entry])
+     VALUES
+           (''
+           ,'')
+GO
+
+INSERT INTO [aviblog].[dbo].[Settings]
+           ([Key]
+           ,[Entry])
+     VALUES
+           (''
+           ,'')
+GO
+
+INSERT INTO [aviblog].[dbo].[Settings]
+           ([Key]
+           ,[Entry])
+     VALUES
+           (''
+           ,'')
+GO
+
